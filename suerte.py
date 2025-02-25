0d0506dd53f429c5796bc89b102010b6eaa3df3e
Lara <tutoriasklen@outlook.com>
2025-02-25 18:04:18 -0500

    numro de la suerte

---
+++ suerte.py
@@ -0,0 +1,9 @@
+# Número de la suerte de dos dígitos del 00 al 99
+
+import random
+
+print("El número de la suerte es:", (random.randint(00, 99)))
+
+# El billete de la suerte de cuatro dígitos del 0000 al 9999
+
+print("El billete de la suerte es:", (random.randint(0000, 9999)))
