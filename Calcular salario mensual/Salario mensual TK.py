bbfc1271ad818bd968409355ed4fe2237d1968d5
Lara <tutoriasklen@outlook.com>
2025-03-08 21:22:40 -0500

    2 decimales update

--- Calcular salario mensual/Salario mensual TK.py
+++ Calcular salario mensual/Salario mensual TK.py
@@ -10,7 +10,7 @@ dias = float(input("Ingrese el numero de días laborados: "))
 horas = 8
 costo = 2.77
             
-print ("Salario mensual: B/", (dias*horas*costo))
+print ("Salario mensual: B/", "%.2f" % (dias*horas*costo))
 
 salario_Emp_1 = (dias*horas*costo)
 
@@ -19,17 +19,17 @@ dias = float(input("Ingrese el numero de días laborados: "))
 horas = 8
 costo = 2.91
             
-print ("Salario mensual: B/", (dias*horas*costo))
+print ("Salario mensual: B/", "%.2f" % (dias*horas*costo))
 
 salario_Emp_2 = (dias*horas*costo)
 
-print ("Total de salario de los dos empledos: ", (salario_Emp_1 + salario_Emp_2))
+print ("Total de salario de los dos empledos: ", "%.2f" % (salario_Emp_1 + salario_Emp_2))
 
 Riesgos_Prof = (salario_Emp_1+salario_Emp_2)*float(0.0098)
 Seguro_Edu = (salario_Emp_1+salario_Emp_2)*float(0.0275)
 Seguro_Social = (salario_Emp_1+salario_Emp_2)*float(0.36656)
 
-print ("Cuota mensual CSS: ", (Riesgos_Prof+Seguro_Edu+Seguro_Social))
+print ("Cuota mensual CSS: ", "%.2f" % (Riesgos_Prof+Seguro_Edu+Seguro_Social))
 
 print("No hay mas empleados")
 exit()
